d45780c596f3d9f6d8d20c5e507e25347826abe9
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2021-01-11 13:42:18 +0600

    Home Route Added

--- UI/src/app.ts
+++ UI/src/app.ts
@@ -1,3 +1,18 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+import { Aurelia, PLATFORM } from 'aurelia-framework';
+
 export class App {
-  public message = 'Hello World!';
+  router: Router;
+
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId:  PLATFORM.moduleName('components/home/index') },
+      // { route: 'users',            name: 'users',      moduleId: 'users/index', nav: true, title: 'Users' },
+      // { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
+      // { route: 'files/*path',      name: 'files',      moduleId: 'files/index', nav: 0,    title: 'Files', href:'#files' }
+    ]);
+  }
 }
