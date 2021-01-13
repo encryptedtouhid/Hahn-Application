8250fad146987f20ab4d053c70b4133c416d2335
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2021-01-13 23:20:28 +0600

    Localization json added

--- UI/src/components/home/index.ts
+++ UI/src/components/home/index.ts
@@ -1,4 +1,17 @@
-import {BindingEngine} from 'aurelia-framework';
+import { BindingEngine } from 'aurelia-framework';
+const importedData = require('locales/transalation.json');
 export class index {
   public message = 'Add Applicants';
+  public language = "EN";
+  public lngdata = null;
+  onClick(button) {
+    if (this.language === "DE") {
+      this.language = "EN";
+    }
+    else {
+      this.language = "DE"
+    }
+    this.lngdata = importedData;
+    console.log('JSON loaded via import', this.lngdata);
+  }
 }
