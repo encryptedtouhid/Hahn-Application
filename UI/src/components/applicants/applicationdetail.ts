6572851f2a74db337e959d420ab4bc5a297e1611
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2021-01-14 12:57:47 +0600

    Updated

--- UI/src/components/applicants/applicationdetail.ts
+++ UI/src/components/applicants/applicationdetail.ts
@@ -1,15 +1,19 @@
 import { autoinject } from "aurelia-framework";
 import { HttpClient } from "aurelia-fetch-client";
-
+const importedData = require('locales/transalation.json');
 @autoinject
 export class ApplicationDetail {
   public heading: string = "Applicant Detail";
   public applicant: any;
+  public language = "EN";
+  public lngdata = null;
 
   constructor(private http: HttpClient) {
     http.configure((config) => {
       config.useStandardConfiguration().withBaseUrl("https://api.github.com/");
     });
+    this.language = "EN";
+    this.lngdata = importedData;
   }
 
   async activate(params, routeConfig): Promise<void> {
@@ -17,5 +21,16 @@ export class ApplicationDetail {
       `${params.url}`
     );
     this.applicant = await response.json();
+  }
+
+  onClick(button) {
+    if (this.language === "DE") {
+      this.language = "EN";
+    }
+    else {
+      this.language = "DE"
+    }   
+    this.lngdata = importedData;
+    console.log('JSON loaded via import', this.lngdata);
   }
 }
